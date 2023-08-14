789ce9fd45021024deeb19603b84546e6c3ab410
Jerome Ghartey <gharteyjerome@gmail.com>
2023-08-14 19:54:10 +0100

    added schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,39 +2,45 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...index]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
-import { myTheme } from './theme'
-import StudioNavbar from './components/StudioNavbar'
-import Logo from './components/Logo'
-import { getDefaultDocumentNode } from './structure'
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schema";
+import { myTheme } from "./theme";
+import StudioNavbar from "./components/StudioNavbar";
+import Logo from "./components/Logo";
+import { getDefaultDocumentNode } from "./structure";
+import { schemaTypes } from "./schemas";
 
 export default defineConfig({
-  basePath: '/studio',
-  name:"Next_Sanity_Studio",
-  title:"Next Sanity Studio",
+  basePath: "/studio",
+  name: "Next_Sanity_Studio",
+  title: "Next Sanity Studio",
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
-  schema,
   plugins: [
     deskTool({
       defaultDocumentNode: getDefaultDocumentNode,
     }),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
   ],
+  schema: {
+    types: schemaTypes,
+  },
+  icon: Logo,
+  logo: Logo,
+  subtitle: "Login to manage the Blog",
   studio: {
     components: {
       logo: Logo,
-      navbar: StudioNavbar
-    }
+      navbar: StudioNavbar,
+    },
   },
-  theme: myTheme
-})
+  theme: myTheme,
+});
---
+++ schemas/index.ts
@@ -0,0 +1,6 @@
+import blockContent from './blockContents'
+import category from './category'
+import post from './post'
+import author from './author'
+
+export const schemaTypes = [post, author, category, blockContent]
